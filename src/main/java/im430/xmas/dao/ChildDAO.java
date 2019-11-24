19e8a3d5d9f23c27705ea35c1c149ddddc801abf
Manuel Leibetseder <leibetseder.manuel@fh-hagenberg.at>
2019-11-24 16:33:26 +0100

    created dao and corresponding xmls

---
+++ src/main/java/im430/xmas/dao/ChildDAO.java
@@ -0,0 +1,16 @@
+package im430.xmas.dao;
+
+import im430.xmas.business.Child;
+import im430.xmas.business.Gift;
+
+import java.util.List;
+
+public interface ChildDAO {
+    void addChild(Child child);
+    void removeChild(Child child);
+    void updateChild(Child child);
+    Child getChildById(int id);
+    List<Child> getAllChildren();
+    void addGift(Child child, Gift gift); // save unpersisted gifts automatically
+    void removeGift(Child child, Gift gift);
+}
---
+++ src/main/java/im430/xmas/dao/ChildDAOImpl.java
@@ -0,0 +1,60 @@
+package im430.xmas.dao;
+
+import im430.xmas.business.Child;
+import im430.xmas.business.Gift;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Resource
+@Transactional
+public class ChildDAOImpl implements ChildDAO {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+
+    public void addChild(Child child) {
+        this.sqlSession.insert("im430.xmas.dao.ChildDAOImpl.addChild", child);
+    }
+
+    public void removeChild(Child child) {
+        this.sqlSession.delete("im430.xmas.dao.ChildDAOImpl.removeChild", child);
+        child.setId(-1);
+    }
+
+    public void updateChild(Child child) {
+        this.sqlSession.update("im430.xmas.dao.ChildDAOImpl.updateChild", child);
+    }
+
+    @Transactional(readOnly=true)
+    public Child getChildById(int id) {
+        return this.sqlSession.selectOne("im430.xmas.dao.ChildDAOImpl.getChildById", id);
+    }
+
+    @Transactional(readOnly=true)
+    public List<Child> getAllChildren() {
+        return this.sqlSession.selectList("im430.xmas.dao.ChildDAOImpl.getAllChildren");
+    }
+
+    public void addGift(Child child, Gift gift) {
+        Map<String, Object> sqlParameters = new HashMap<String, Object>();
+        sqlParameters.put("child", child);
+        sqlParameters.put("gift", gift);
+
+        this.sqlSession.insert("im430.xmas.dao.ChildDAOImpl.addGift",
+                sqlParameters);
+        child.getGifts().add(gift);
+    }
+
+    public void removeGift(Child child, Gift gift) {
+
+        this.sqlSession.delete("im430.xmas.dao.ChildDAOImpl.removeGift", gift);
+        child.getGifts().remove(gift);
+    }
+}
